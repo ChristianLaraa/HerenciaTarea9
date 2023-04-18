238bf51f21a6800c3298f2fe1773a4a76741a43a
ChristianLaraa <christian.lara.091204@gmail.com>
2023-04-17 23:55:24 -0600

    Herencia Tarea 9

---
+++ Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Taxi taxi1 = new Taxi("Nissa", "Maech", 2015, 4, 5, "Apagado", "Neutral", "A-6867-A", "Jose Lopez", 0);
+        Taxi taxi2 = new Taxi("Chevrolet", "Chevvy", 2012, 2, 5, "Encendido", "Retrocediendo...", "A-0108-J", "Calos Perez", 4);
+
+        System.out.println("");
+        System.out.println(taxi1.toString());
+        System.out.println("");
+        System.out.println(taxi2.toString());
+        System.out.println("");
+
+        taxi1.encender();
+        System.out.println("");
+        taxi2.apagar();
+        System.out.println("");
+        taxi1.Avanzar();
+        System.out.println("");
+        taxi1.Reversa();
+        System.out.println("");
+        taxi2.Neutral();
+        System.out.println("");
+        taxi1.subirPasajeros();
+        System.out.println("");
+        taxi2.bajarPasajeros();
+    }
+}
